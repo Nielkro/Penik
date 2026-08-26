75084138dbdf2b68c8e3f7edb165be3b8537f5c9
gtinikita203 <gtinikita2031@proton.me>
2026-08-26 20:25:55 +0500

    feat(landing): link to web.penik.ru and enable direct APK download buttons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
             // MessagePack, LiveKit/WebRTC JNI, Hilt).
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
